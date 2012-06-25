1fceaeec4b97019bbcdaf0cc3622736315db574a
Vincent Agnano <vincent.agnano@particul.es>
2012-06-25 18:27:07 +0200

    cleaned urls import

--- imagefit/urls.py
+++ imagefit/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls.shortcut import urlpatterns
-from django.conf.urls import url, include, patterns
+from django.conf.urls import url, patterns
 
 urlpatterns = patterns('',
     url(r'^resize/(?P<format>[\w-]+)/(?P<url>.*)$', 'imagefit.views.resize', name="imagefit_resize"),
