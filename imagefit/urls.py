e0bebfc9586e5a62acfd89d4f3ccbfc41b1461eb
bencipher <femolak@gmail.com>
2022-05-30 18:14:47 +0200

    version check for latest django version to check path compliance

--- imagefit/urls.py
+++ imagefit/urls.py
@@ -1,9 +1,20 @@
-from django.urls import re_path
 from . import views
 
+# EAFP compliance with version 4.0
+try:
+    from django.conf.urls import url
+except ImportError as e:
+    from django.urls import re_path
+    from django.utils import version
+
+    # in case of any other error
+    if int(version.get_version().split('.')[0]) >= 4:
+        url = re_path
+    else:
+        raise ImportError(str(e))
 
 urlpatterns = [
-    re_path(
+    url(
         r'^(?P<path_name>[\w_-]*)/(?P<format>[,\w-]+)/(?P<url>.*)/?$',
         views.resize,
         name="imagefit_resize"),
