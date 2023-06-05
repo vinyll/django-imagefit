d7fc51d35fe47b6296e52ae31886ee9dee629c70
Vincent Agnano <vinyll@protonmail.com>
2023-06-05 23:22:29 +0200

    preparing for pypi

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-imagefit',
     version='0.7.0',
+    long_description_content_type='text/markdown',
     description='Render an optimized version of your original image on display. Ability to resize and crop.',
     long_description=open('README.md').read(),
     author='Vincent Agnano',
